0946b1f8dfdb9606c4e3f98aae0e99b8da51e338
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-07 13:09:11 +0200

    feat metido archivo render

--- server.js
+++ server.js
@@ -334,6 +334,7 @@ const io = new Server(server, {
     credentials: true
   }
 })
+app.set('trust proxy', true)
 
 const uploadDir = path.join(__dirname, 'uploads')
 if (!fs.existsSync(uploadDir)) {
