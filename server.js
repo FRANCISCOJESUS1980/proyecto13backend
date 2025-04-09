a2736dcbae276ddf738a03e9c655aec6ff116c60
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-09 17:11:44 +0200

    feat probando render

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ const mensajesPrivadosRoutes = require('./src/routes/mensajesPrivadosRoutes')
 
 const io = new Server(server, {
   cors: {
-    origin: /*'https://proyecto13fronted.vercel.app'*/ 'http://localhost:5173',
+    origin: 'https://proyecto13fronted.vercel.app' /* 'http://localhost:5173'*/,
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -180,7 +180,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: /*'https://proyecto13fronted.vercel.app'*/ 'http://localhost:5173',
+    origin: 'https://proyecto13fronted.vercel.app' /*'http://localhost:5173'*/,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
