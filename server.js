bdf4cff9279fffd1b007de3e4c915ab22edbd48a
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-09 17:18:43 +0200

    feat probando render

--- server.js
+++ server.js
@@ -178,14 +178,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(
-  cors({
-    origin: 'https://proyecto13fronted.vercel.app' /*'http://localhost:5173'*/,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  })
-)
+app.use(cors())
 
 app.use(
   helmet({
@@ -312,3 +305,9 @@ server.listen(PORT, () =>
 )
 
 module.exports = app
+/*{
+  origin: 'https://proyecto13fronted.vercel.app' 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }*/
