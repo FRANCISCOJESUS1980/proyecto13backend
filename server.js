8a2eb5fc16b5951a5a647d0c657e7f63118dc9b7
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-11 10:03:02 +0200

    feat probando render

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ const mensajesPrivadosRoutes = require('./src/routes/mensajesPrivadosRoutes')
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://proyecto13fronted.vercel.app' /* 'http://localhost:5173'*/,
+    origin: /*'https://proyecto13fronted.vercel.app' */ 'http://localhost:5173',
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -178,7 +178,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(cors())
+app.use(
+  cors({
+    origin: /* 'https://proyecto13fronted.vercel.app'*/ 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+)
 
 app.use(
   helmet({
@@ -187,14 +194,14 @@ app.use(
 )
 /*app.set('trust proxy', 1)*/
 
-/*const limiter = rateLimit({
+const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 500,
   message: 'Demasiadas solicitudes, intenta de nuevo más tarde',
   standardHeaders: true,
   legacyHeaders: false
 })
-app.use('/api/', limiter)*/
+app.use('/api/', limiter)
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
@@ -306,9 +313,3 @@ server.listen(PORT, () =>
 )
 
 module.exports = app
-/*{
-  origin: 'https://proyecto13fronted.vercel.app' 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }*/
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -16,7 +16,7 @@ const {
   verificarCodigo
 } = require('../controllers/userController')
 
-router.use((req, res, next) => {
+/*router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
@@ -30,7 +30,7 @@ router.use((req, res, next) => {
   }
 
   next()
-})
+})*/
 
 router.get('/entrenadores', async (req, res) => {
   try {
