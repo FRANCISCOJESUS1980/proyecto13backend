49419ff98223ca11dca42adb13f0ccaadad4aedb
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-11 21:32:31 +0200

    feat probando render

--- server.js
+++ server.js
@@ -163,8 +163,7 @@ const mensajesPrivadosRoutes = require('./src/routes/mensajesPrivadosRoutes')
 
 const io = new Server(server, {
   cors: {
-    origin:
-      'https://proyecto13backend.onrender.com/api' /*'https://proyecto13fronted.vercel.app'*/ /* 'http://localhost:5173'*/,
+    origin: 'https://proyecto13fronted.vercel.app' /* 'http://localhost:5173'*/,
     methods: ['GET', 'POST'],
     credentials: true
   }
