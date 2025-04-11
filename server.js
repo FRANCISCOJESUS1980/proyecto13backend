4f3baa76f7fa973244d02b554752db2f1ddac9cd
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-11 21:25:14 +0200

    feat probando render

--- server.js
+++ server.js
@@ -180,7 +180,8 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: 'https://proyecto13fronted.vercel.app' /*'http://localhost:5173'*/,
+    origin:
+      'https://proyecto13backend.onrender.com/api' /*'https://proyecto13fronted.vercel.app'*/ /*'http://localhost:5173'*/,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
