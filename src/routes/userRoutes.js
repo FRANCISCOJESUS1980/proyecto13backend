82128982c07c972061ad23f9ffa9e9a64146f596
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-04-11 10:16:13 +0200

    feat probando render

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -17,7 +17,10 @@ const {
 } = require('../controllers/userController')
 
 router.use((req, res, next) => {
-  /* res.header('Access-Control-Allow-Origin', 'http://localhost:5173')*/
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://vercel.com/francisco-jesus-projects/proyecto13fronted' /*'http://localhost:5173'*/
+  )
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
     'Access-Control-Allow-Headers',
